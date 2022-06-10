2177200fa7a328bdd46bb613743215d14980f89b
Syimyk25 <csqvsr25@gmail.com>
2022-06-10 17:04:02 +0600

    new file

---
+++ task.py
@@ -0,0 +1,56 @@
+"""
+#1
+ctrl + alt +t
+
+#2
+git help
+
+#3
+mkdir test
+touch test/main.py
+nano test/main.py
+def hello(): print("Hello World!")
+cd test
+git init
+
+#4
+git add .
+
+#5
+git commit -m "my first commit"
+
+#6
+git status
+
+#7
+git log
+
+#8
+git branch mybranch
+
+#9
+git checkout mybranch
+git status
+
+#10
+nano main.py
+def hello(): print("Python is awesome!")
+
+#11
+git commit -m "Исправил функцию, добавил новое сообщение"
+
+#12
+git checkout master
+git status
+
+#13
+git merge mybranch
+
+#14
+git remote add origin git@github.com:student123/MyRepo.git
+
+#15
+git push origin master
+
+"""
+
